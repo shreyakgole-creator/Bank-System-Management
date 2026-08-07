b64cbfe4beaa38ec9b09a3267fe69ef747685ddf
Shreya Gole <shreyagole1002@gmail.com>
2026-08-07 11:09:48 +0530

    Merge branch 'master' of https://github.com/shreyakgole-creator/Bank-System-Management

---
+++ src/main/java/dao/AccountDao.java
@@ -0,0 +1,53 @@
+package dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class AccountDao
+{
+    private EntityManager em;
+    public  AccountDao(EntityManager em)
+    {
+        this.em=em;
+    }
+    public  void  deposit(int accountId,double amount)
+    {
+        EntityTransaction tx=em.getTransaction();
+        tx.begin();
+
+        Account account=em.find(Account.class,accountId);
+
+        account.setBalance(account.getBalance()+amount);
+
+        Transaction t=new Transaction("Deposit",amount);
+        t.setAccount(account);
+
+        em.persist(t);
+
+        tx.commit();
+
+    }
+
+    public void withdraw(int accountId,double amount){
+
+        EntityTransaction tx=em.getTransaction();
+        tx.begin();
+
+        Account account=em.find(Account.class,accountId);
+
+        if(account.getBalance()>=amount){
+
+            account.setBalance(account.getBalance()-amount);
+
+            Transaction t=new Transaction("Withdraw",amount);
+            t.setAccount(account);
+
+            em.persist(t);
+
+        }
+
+        tx.commit();
+
+
+    }
+}
